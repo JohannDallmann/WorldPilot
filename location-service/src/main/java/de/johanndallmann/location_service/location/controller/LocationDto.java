bd9a605b429b4b3172f52a8fc92d48b03983d725
Johann Dallmann <JDallmann@conet.de>
2025-06-18 16:29:16 +0200

    change LocationDto from class to record

--- location-service/src/main/java/de/johanndallmann/location_service/location/controller/LocationDto.java
+++ location-service/src/main/java/de/johanndallmann/location_service/location/controller/LocationDto.java
@@ -1,18 +1,13 @@
 package de.johanndallmann.location_service.location.controller;
 
 import de.johanndallmann.location_service.common.enums.LocationType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class LocationDto {
-    private long id;
-    private String name;
-    private LocationType type;
-    private String city;
-    private String country;
-    private String description;
+public record LocationDto(
+    long id,
+    String name,
+    LocationType type,
+    String city,
+    String country,
+    String description
+) {
 }
--- location-service/src/main/java/de/johanndallmann/location_service/location/controller/LocationFilterDto.java
+++ location-service/src/main/java/de/johanndallmann/location_service/location/controller/LocationFilterDto.java
@@ -1,7 +1,5 @@
 package de.johanndallmann.location_service.location.controller;
 
-import de.johanndallmann.location_service.common.enums.LocationType;
-
 public record LocationFilterDto(
         String city,
         String country,
