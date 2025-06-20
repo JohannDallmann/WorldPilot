fa2af10c0cba338ae594ecd516f0c624e566f11c
Johann Dallmann <JDallmann@conet.de>
2025-06-20 13:01:44 +0200

    swagger added, controller-documentation adjusted

--- location-service/src/main/java/de/johanndallmann/locationService/location/controller/LocationController.java
+++ location-service/src/main/java/de/johanndallmann/locationService/location/controller/LocationController.java
@@ -3,6 +3,10 @@ package de.johanndallmann.locationService.location.controller;
 import de.johanndallmann.locationService.common.mapper.LocationControllerMapper;
 import de.johanndallmann.locationService.location.service.Location;
 import de.johanndallmann.locationService.location.service.LocationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/locations")
 @RequiredArgsConstructor
+@Tag(name = "Locations", description = "API for Location-operationes")
 public class LocationController {
 
     private final LocationService locationService;
@@ -26,25 +31,43 @@ public class LocationController {
         return ResponseEntity.ok(this.locationControllerMapper.toDtoList(locationList));
     }
 
-    /**
-     * Handles Get-request for locations and maps into LocationDto
-     * @param filter contains all filter-attributes
-     * @param pageable contains informations about pagenumber, pagesize and sorting
-     *                 (e.g. "/locations?page=1&size=3&sort=id,desc")
-     * @return page-content of all locations matching the filter-attributes
-     */
+    @Operation(
+            summary = "Returns a selection of locations",
+            description = "Expects a LocationFilterDto as RequestBody with a valid LocationType " +
+                    "and a Pageable Object in the URL which contains pagenumber, pagesize and sorting " +
+                    "(e.g. \"/locations?page=1&size=3&sort=id,desc\"). " +
+                    "Returns page-content of all locations matching the filter-attributes."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Locations successfully returned"),
+            @ApiResponse(responseCode = "400", description = "Invalid LocationFilterDto"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping
     public ResponseEntity<List<LocationDto>> getLocationPage(@RequestBody LocationFilterDto filter, Pageable pageable) {
         Page<Location> locationPage = this.locationService.getLocationPage(filter, pageable);
         return ResponseEntity.ok(locationPage.map(locationControllerMapper::toDto).getContent());
     }
 
+    @Operation(
+            summary = "Creates a new location",
+            description = "Expects a NewLocationDto as RequestBody with a valid LocationType " +
+                    "and returns the ID of the created Location."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Location successfully created"),
+            @ApiResponse(responseCode = "400", description = "Invalid NewLocationDto"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @PostMapping
     public ResponseEntity<Void> postNewLocation(@RequestBody NewLocationDto newLocation){
         Location savedLocation = this.locationService.createNewLocation(this.locationControllerMapper.newLocationToDomain(newLocation));
         return responseEntityWithLocation(savedLocation.getId());
     }
 
+    /**
+     * Creates the Uri based on the current request and the resourceId and wraps it with a ResponseEntity
+     */
     private ResponseEntity<Void> responseEntityWithLocation(Object resourceId) {
         URI resourceLocation = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{childId}").buildAndExpand(resourceId)
                 .toUri();
