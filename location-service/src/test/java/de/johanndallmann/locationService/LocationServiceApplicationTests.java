22ef58fa1494a08c07d235dbbcd88b95f8c5e029
Johann Dallmann <JDallmann@conet.de>
2025-06-20 13:17:14 +0200

    add profile to test

--- location-service/src/test/java/de/johanndallmann/locationService/LocationServiceApplicationTests.java
+++ location-service/src/test/java/de/johanndallmann/locationService/LocationServiceApplicationTests.java
@@ -2,8 +2,10 @@ package de.johanndallmann.locationService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LocationServiceApplicationTests {
 
 	@Test
